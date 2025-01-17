547ed9d5cbc14ed01bc0e207de2ff509eabb926a
timothyAgevi <timothyagevi@gmail.com>
2025-01-17 18:04:34 +0300

    cleanup

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { ChatBot } from "./components/ChatBot";
-export function App() {
-  return (
-    <div className="h-screen bg-[#0E1621]">
-      <ChatBot />
-    </div>
-  );
-}
